ca179aedff45f70a2ea53a146a21f8bef7626461
“Sanjana <sbsanjana@gmail.com>
2022-01-01 18:54:00 -0500

    text  conversion

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Upload extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedFile: null
+        };
+    }
+    onChangeHandler = (event) => {
+        this.setState({
+            selectedFile: event.target.files[0],
+            loaded: 0,
+        });
+        console.log(event.target.files[0]);
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        const { selectedFile } = this.state;
+        formData.append('inputFile', selectedFile);
+        console.log(formData)
+        fetch('http://127.0.0.1:5000/upload', {
+            method: 'POST',
+            body: formData,
+        });
+    };
+
+    render() {
+        return (
+            < div >
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Upload a file: <br /><br />
+                        <input type="file" name="file" onChange={this.onChangeHandler} />
+                    </label>
+                    <br /><br />
+                    <button type="submit">
+                        Upload
+                    </button>
+                </form >
+            </div >
+        );
+    }
 }
-
-export default App;
